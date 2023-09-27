9ff753db714c27a441dc3884a0de41ec0c7d9d0e
Amos Lucky <aluckya100@gmail.com>
2023-09-27 21:53:19 +0100

    first commit

---
+++ about.php
@@ -0,0 +1,280 @@
+<?php
+
+echo strlen("The word cargo refers in particular to goods or produce being conveyed generally for commercial gain by ship, boat, or aircraft, although the term is now often extended to cover all types of freight, including that carried by train, van, truck, or intermodal container.");
+echo "";
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from www.exotheme.com/gocargo/about.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 25 Sep 2023 10:24:55 GMT -->
+<head>
+    <meta charset="utf-8">
+    <title>GoCargo - Freight, Logistics & Transportation Website Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- LOAD CSS FILES -->
+    <link href="css/main.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <div id="preloader"></div>
+    <div id="wrapper">
+        <!-- header begin -->
+        <header>
+            <div class="container">
+                <span id="menu-btn"></span>
+                <div class="row">
+                    <div class="col-md-3">
+                        <!-- logo begin -->
+                        <div id="logo">
+                            <div class="inner">
+                                <a href="index.html">
+                                    <img src="img/logo.png" alt="" class="logo-1">
+                                    <img src="img/logo-2.png" alt="" class="logo-2">
+                                </a>
+
+                            </div>
+                        </div>
+                        <!-- logo close -->
+                    </div>
+
+                    <div class="col-md-9">
+                        <!-- mainmenu begin -->
+                        <nav id="mainmenu-container">
+                            <ul id="mainmenu">
+                                <li><a href="index.html">Home</a>
+                                    <ul>
+                                        <li><a href="index.html">Homepage 1</a></li>
+                                        <li><a href="index-2.html">Homepage 2</a></li>
+                                        <li><a href="index-3.html">Homepage 3</a></li>
+                                        <li><a href="index-4.html">Homepage 4</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="services.html">Services</a>
+                                    <ul>
+                                        <li><a href="services.html">All Services</a></li>
+                                        <li><a href="service-details-1.html">Ocean Freight</a></li>
+                                        <li><a href="service-details-2.html">Air Freight</a></li>
+                                        <li><a href="service-details-3.html">Cargo Express</a></li>
+                                        <li><a href="service-details-4.html">Logistics</a></li>
+                                        <li><a href="service-details-5.html">Warehousing</a></li>
+                                        <li><a href="service-details-6.html">Custom Brokerage</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="about.html">About Us</a></li>
+                                <li><a href="track.html">Track</a></li>
+                                <li><a href="news.html">News</a></li>
+                                <li><a href="gallery.html">Gallery</a></li>
+                                <li><a href="contact.html">Contact</a></li>
+                            </ul>
+                        </nav>
+                        <!-- mainmenu close -->
+
+                        <!-- social icons -->
+                        <div class="social">
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                            <a href="#"><i class="fa fa-rss"></i></a>
+                            <a href="#"><i class="fa fa-google-plus"></i></a>
+                            <a href="#"><i class="fa fa-envelope-o"></i></a>
+                        </div>
+                        <!-- social icons close -->
+                    </div>
+
+                </div>
+            </div>
+        </header>
+        <!-- header close -->
+
+        <!-- subheader begin -->
+        <section id="subheader" class="page-about no-bottom" data-stellar-background-ratio="0.5">
+            <div class="overlay">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h1>About Us
+							<span>Fast International Cargo Services</span>
+                            </h1>
+                            <div class="small-border wow flipInY" data-wow-delay=".8s" data-wow-duration=".8s"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- subheader close -->
+
+        <div class="clearfix"></div>
+
+        <!-- content begin -->
+        <div id="content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <img src="img/pic-1.jpg" class="img-responsive" alt="">
+                        <div class="divider-single"></div>
+                        <h2>About us</h2>
+                        <div class="tiny-border"></div>
+                        <p>
+                            Welcome to Global Shipping Experts, your trusted partner in international logistics and
+                             freight forwarding. With a passion for excellence and a commitment to delivering 
+                            top-notch shipping solutions, we are your gateway to seamless global trade.</p>
+
+                    </div>
+                    <div class="col-md-6">
+                        <img src="img/pic-2.jpg" class="img-responsive" alt="">
+                        <div class="divider-single"></div>
+                        <h2>Our mission</h2>
+                        <div class="tiny-border"></div>
+
+                        <p>
+                            At Global Shipping Experts, our mission is clear: to empower businesses and
+                             individuals with efficient, reliable, and cost-effective shipping solutions 
+                             that transcend borders. We believe in simplifying complex logistics 
+                             challenges and providing our clients with the
+                             confidence to explore new markets and opportunities worldwide.
+                        </p>
+                    </div>
+                    
+
+                    
+
+                    <div class="text-center">
+                        <h2 class="wow fadeInUp" data-wow-delay="0">Why Choose Us</h2>
+                        <div class="small-border wow flipInY" data-wow-delay=".2s" data-wow-duration=".8s"></div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="feature-box">
+                            <i class="icon-tools wow zoomIn" data-wow-delay="0s"></i>
+                            <div class="text wow fadeIn" data-wow-delay=".2s">
+                                <h3>Expertise and Experience: </h3>
+                                <p>
+                                    With years of experience in the shipping industry, Global Shipping Experts brings a wealth of knowledge and expertise to the table. Our team of professionals understands the intricacies of international logistics, customs regulations, and supply chain management.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div class="feature-box">
+                            <i class="icon-bargraph wow zoomIn" data-wow-delay=".2s"></i>
+                            <div class="text wow fadeIn" data-wow-delay=".4s">
+                                <h3>Comprehensive Solutions: </h3>
+                                <p>
+                                    We offer a one-stop-shop for all your shipping needs. Whether you require air, ocean, or land freight services, customs clearance, warehousing, e-commerce shipping, or specialized project cargo solutions, we have you covered. Our diverse range of services caters to businesses of all sizes and industries.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div class="feature-box">
+                            <i class="icon-pricetags wow zoomIn" data-wow-delay=".4s"></i>
+                            <div class="text wow fadeIn" data-wow-delay=".6s">
+                                <h3>Global Network:</h3>
+                                <p>Thanks to our extensive network of partners and agents worldwide, we can seamlessly connect your business to markets across the globe. Whether you're expanding into new territories or consolidating your presence in existing markets, our global reach ensures your cargo reaches its destination reliably and efficiently.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div class="feature-box">
+                            <i class="icon-refresh wow zoomIn" data-wow-delay=".6s"></i>
+                            <div class="text wow fadeIn" data-wow-delay=".8s">
+                                <h3>Customs Expertise: </h3>
+                                <p>Navigating the complexities of customs clearance can be a daunting task. Our experienced customs experts will handle all documentation and compliance matters, ensuring that your cargo moves smoothly through customs, avoiding unnecessary delays and penalties.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- content close -->
+
+        <!-- footer begin -->
+        <footer>
+            <div class="container">
+                <div class="row">
+
+
+
+                    <div class="col-md-4">
+                        <h3>Our Services</h3>
+                        <div class="tiny-border"></div>
+                        <ul>
+                            <li><a href="#">Ocean Freight</a></li>
+                            <li><a href="#">Sky Freight</a></li>
+                            <li><a href="#">Cargo Express</a></li>
+                            <li><a href="#">Logistics</a></li>
+                            <li><a href="#">Warehousing</a></li>
+                            <li><a href="#">Custom Brokerage</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-md-4">
+                        <h3>Location</h3>
+                        <div class="tiny-border"></div>
+                        Somewhere Over Rainbow<br>
+                        Green World Rainbow Green World<br>
+                        T. 0908 1000 1000<br>
+                        E. contact@exotheme.com<br>
+
+                        <div class="divider-single"></div>
+                        <div class="social-icons">
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                            <a href="#"><i class="fa fa-google-plus"></i></a>
+                            <a href="#"><i class="fa fa-rss"></i></a>
+                            <a href="#"><i class="fa fa-envelope-o"></i></a>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <h3>Buy Now</h3>
+                        <div class="tiny-border"></div>
+                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                        <a href="#" class="btn-border-light">Buy Now</a>
+                    </div>
+
+
+
+                </div>
+            </div>
+            <div class="divider"></div>
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <div class="spacer-single"></div>
+                        &copy; Copyright 2015 - GoCargo by ExoTheme
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- footer close -->
+    </div>
+
+    <!-- LOAD JS FILES -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/easing.js"></script>
+    <script src="js/jquery.ui.totop.js"></script>
+    <script src="js/ender.js"></script>
+    <script src="js/owl.carousel.js"></script>
+    <script src="js/jquery.fitvids.js"></script>
+    <script src="js/jquery.plugin.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/jquery.stellar.js"></script>
+    <script src="js/typed.js"></script>
+    <script src="js/jquery.scrollto.js"></script>
+    <script src="js/custom.js"></script>
+
+</body>
+
+<!-- Mirrored from www.exotheme.com/gocargo/about.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 25 Sep 2023 10:25:01 GMT -->
+</html>
---
+++ footer.php
@@ -0,0 +1,87 @@
+ <!-- footer begin -->
+ <footer>
+            <div class="container">
+                <div class="row">
+
+
+
+                    <div class="col-md-4">
+                        <h3>Our Services</h3>
+                        <div class="tiny-border"></div>
+                        <ul>
+                            <li><a href="#">Ocean Freight</a></li>
+                            <li><a href="#">Sky Freight</a></li>
+                            <li><a href="#">Cargo Express</a></li>
+                            <li><a href="#">Logistics</a></li>
+                            <li><a href="#">Warehousing</a></li>
+                            <li><a href="#">Custom Brokerage</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-md-4">
+                        <h3>Location</h3>
+                        <div class="tiny-border"></div>
+                        Somewhere Over Rainbow<br>
+                        Green World Rainbow Green World<br>
+                        T. 0908 1000 1000<br>
+                        E. contact@exotheme.com<br>
+
+                        <div class="divider-single"></div>
+                        <div class="social-icons">
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                            <a href="#"><i class="fa fa-google-plus"></i></a>
+                            <a href="#"><i class="fa fa-rss"></i></a>
+                            <a href="#"><i class="fa fa-envelope-o"></i></a>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <h3>Buy Now</h3>
+                        <div class="tiny-border"></div>
+                        <p>Multi-modal container units, designed as reusable carriers to facilitate unit load handling of the goods contained, are also referred to as cargo, specially by shipping lines and logistics operators.</p>
+                        <a href="#" class="btn-border-light">Buy Now</a>
+                    </div>
+
+                </div>
+            </div>
+            <div class="divider"></div>
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <div class="spacer-single"></div>
+                        &copy; Copyright 2015 - GoCargo by ExoTheme
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- footer close -->
+    </div>
+
+    <!-- LOAD JS FILES -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/easing.js"></script>
+    <script src="js/jquery.ui.totop.js"></script>
+    <script src="js/ender.js"></script>
+    <script src="js/owl.carousel.js"></script>
+    <script src="js/jquery.fitvids.js"></script>
+    <script src="js/jquery.plugin.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/jquery.stellar.js"></script>
+    <script src="js/typed.js"></script>
+    <script src="js/jquery.scrollto.js"></script>
+    <script src="js/custom.js"></script>
+
+    <!-- SLIDER REVOLUTION SCRIPTS  -->
+    <script type="text/javascript" src="rs-plugin/js/jquery.themepunch.plugins.min.js"></script>
+    <script type="text/javascript" src="rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+    <script src="js/revslider-custom.js"></script>
+
+</body>
+
+<!-- Mirrored from www.exotheme.com/gocargo/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 25 Sep 2023 10:23:58 GMT -->
+</html>
